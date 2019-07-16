d28aabeee3938fb0680b650c02f1c7ff3d3cae28
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-16 15:06:59 -0400

    set dynamic port env

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
+app.get('/other', (req, res) => {
+  res.send('yes, other route');
+});
+
+// dynamically figure out what port to listen to
+const PORT = process.env.PORT;
 app.listen(5000);
