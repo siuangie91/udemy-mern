1792b7d0e5d1279fff748b78fe9fd94ccb091c00
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-16 15:07:33 -0400

    default port to 5000

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get('/other', (req, res) => {
 });
 
 // dynamically figure out what port to listen to
-const PORT = process.env.PORT;
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
