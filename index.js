e59b991bd9858e793edc4d1e15304cd1b49bcc97
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-16 15:39:22 -0400

    remove /other route

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.get('/other', (req, res) => {
-  res.send('yes, other route');
-});
-
 // dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
