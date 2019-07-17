33dca935c21163539d09a928f1a1515f51a15ec9
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-17 16:14:13 -0400

    set up mongoose

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 require('./services/passport'); // just need to run the file, so no need to assign to a var
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
