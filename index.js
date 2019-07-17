1266afdcf59124b67269d5da58a7d3f164059adb
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-17 10:41:03 -0400

    install passport and google oauth stragegy

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 // dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
