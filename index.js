2d2817413c02777b390a767c6e9cd24ef85d91b1
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-07-17 12:20:26 -0400

    set up auth route handler

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ const keys = require('./config/keys');
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+  new GoogleStrategy( // will be known as a string value of 'google'
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback' // route user will be sent to after they grant our app permission
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// when user gets to this route, kick them into the passport flow
+app.get(
+  '/auth/google/',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] // specifies to google what type of access we want from the user's info
   })
 );
 
 // dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// http://localhost:5000/auth/google/callback
