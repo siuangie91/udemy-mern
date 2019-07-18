be52c66eee5980cad0d580d36f2a53e531847eaa
Siu <210014@Ansi8039-MKT.local>
2019-07-18 12:50:10 -0400

    allow proxy

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback' // route user will be sent to after they grant our app permission
+      callbackURL: '/auth/google/callback', // route user will be sent to after they grant our app permission
+      proxy: true // allow proxy
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
