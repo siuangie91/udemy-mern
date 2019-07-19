ceeabfc5b1b4e46fc9d7f0007496477e4a328f97
Siu <210014@Ansi8039-MKT.local>
2019-07-19 14:07:47 -0400

    add login logic to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return; // show nothing if still deciding if logged in or not
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Log in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +28,17 @@ class Header extends Component {
           <a href='' className='left brand-logo'>
             Emaily
           </a>
-          <ul className='right'>
-            <li>
-              <a href=''>Log in with Google</a>
-            </li>
-          </ul>
+          <ul className='right'>{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {
+    auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
