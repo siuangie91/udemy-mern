fa257178574887a7f957760fc1f86bdebc4c555d
Siu <210014@Ansi8039-MKT.local>
2019-07-19 13:58:17 -0400

    return payload for fetch_user action type in auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
+  // default state to `null` to signal that by default we don't know if logged in or not
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; // if not logged in, payload is ""
     default:
       return state;
   }
