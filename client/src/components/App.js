7b906ca0d9f97dc6b0ed61d6c2dc4c40edfcfdd0
Siu <210014@Ansi8039-MKT.local>
2019-07-19 11:38:17 -0400

    set up react router dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello</h1>
-    )
+    return <h1>Hello</h1>;
   }
-};
+}
 
 export default App;
