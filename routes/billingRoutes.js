0d82c492c304fd76bc1c3a51ac2d423e8256a934
Siu <210014@Ansi8039-MKT.home>
2019-07-25 15:40:19 -0400

    create charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
-    
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: 'Add 5 credits for $5',
+      source: req.body.id
+    })
   });
 };
