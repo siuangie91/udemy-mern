e68deabaa8061c277c98b570362b3b308ca951c0
Siu <210014@Ansi8039-MKT.home>
2019-07-25 14:38:42 -0400

    skeleton post route for stripe payments

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app); // import the authRoutes function and call it passing in `app`
+require('./routes/billingRoutes')(app);
 
 // dynamically figure out what port to listen to
 const PORT = process.env.PORT || 5000;
