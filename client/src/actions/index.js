3e3a9a1771ef0eb50149327f84e60940e1923000
Siu <210014@Ansi8039-MKT.home>
2019-07-25 13:56:51 -0400

    create handleToken action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,10 @@ export const fetchUser = () => async dispatch => {
   const { data } = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: data });
 };
+
+export const handleToken = token => async dispatch => {
+  const { data } = await axios.post('/api/stripe', token);
+  // we are updating the user model since it contains the credit info
+  // therefore we can dispatch the same FETCH_USER action
+  dispatch({ type: FETCH_USER, payload: data });
+};
