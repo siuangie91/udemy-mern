ba29f826b4feeb870c66d59afcf45b681ec06dd2
Siu <210014@Ansi8039-MKT.local>
2019-07-31 16:52:51 -0400

    fix typos

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
   });
 
