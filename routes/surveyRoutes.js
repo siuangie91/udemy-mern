c77df7b88c7042289cb174c11f677f3d7b6c25b2
Siu <210014@Ansi8039-MKT.local>
2019-07-31 16:36:12 -0400

    save survey and deduct credit

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -22,7 +22,19 @@ module.exports = app => {
     });
 
     // send email
-    const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      await mailer.send();
+
+      // save survey
+      await survey.save();
+
+      // deduct user credits
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
