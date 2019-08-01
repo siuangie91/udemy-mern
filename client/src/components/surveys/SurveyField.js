58fd3abff3497b6e4d1ea319e334efea59b640b8
Siu <210014@Ansi8039-MKT.local>
2019-08-01 13:38:33 -0400

    create the other fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label }) => {
   // props.input has all the event handlers
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
     </div>
   );
