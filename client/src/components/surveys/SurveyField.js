132a2f7afa2746ebb20ca579e65f7c535f7e45e6
Siu <210014@Ansi8039-MKT.local>
2019-08-01 15:12:31 -0400

    display error for title

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   // props.input has all the event handlers
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
