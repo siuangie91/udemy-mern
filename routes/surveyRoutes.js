83a25a13b2657ff8faaf483d4274e97333daeade
Siu <210014@Ansi8039-MKT.local>
2019-08-15 16:14:17 -0400

    update lastResponded and thank you route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
@@ -77,7 +77,8 @@ module.exports = app => {
             },
             $set: {
               'recipients.$.responded': true
-            }
+            },
+            lastResponded: new Date()
           }
         ).exec();
       })
