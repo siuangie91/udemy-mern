7fcc2db0d445cea8c05cf8bd6835a20bf87615ee
Siu <210014@Ansi8039-MKT.home>
2019-08-16 14:43:03 -0400

    display surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,8 +6,29 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div key={survey._id} className='card'>
+          <div className='card-content'>
+            <span className='card-title'>{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className='right'>
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
